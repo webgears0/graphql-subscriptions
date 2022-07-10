c69257e8c4e4bdd5b3b67de3722c3cbcd3b4a18a
Matt Gear <web_3_gears@protonmail.com>
2022-07-10 17:26:20 +0100

    createNewPost mutation

--- server/api/resolvers.ts
+++ server/api/resolvers.ts
@@ -1,11 +1,57 @@
+interface BaseMutationRequest {
+  parent: string;
+  context: string;
+}
+
+interface CreatePostRequest {
+  input: CreatePostRequestInput
+}
+
+interface CreatePostRequestInput {
+  title: string;
+  content: string;
+}
+
+interface CreatePostResponse {
+  post: Post
+}
+
+type Post = {
+  title: string;
+  content: string;
+  createdAt: Date;
+  updatedAt?: Date;
+}
+
 function helloWorld() {
   return 'Hello, World!';
+}
+
+function createNewPost({ input }: {
+  input: CreatePostRequestInput
+}) {
+  const { title: rawTitle, content: rawContent } = input;
+  const title = rawTitle.trim();
+  const content = rawContent.trim();
+
+  const post = {
+    title,
+    content,
+    createdAt: new Date(),
+  }
+
+  return { post };
 }
 
 const resolvers = {
   Query: {
     helloWorld,
   },
+  Mutation: {
+    createPost(_parent: unknown, args: CreatePostRequest, _context: unknown): CreatePostResponse {     
+      return createNewPost(args)
+    }
+  }
 };
 
 export default resolvers;
